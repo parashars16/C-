305324f9a62fdc60fc0e36c1b4cd062fb8b0c676
parashars16 <shreyaparashar06@gmail.com>
2025-06-07 15:19:07 +0530

    Add files via upload

---
+++ quick_quiz.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main(){
+      /*Write a program to print natural numbers from 10 to 20 when initial loop 
+counter is initialized to 0. */
+    int i = 0;
+    while (i < 11) {
+        printf("%d ", 10 + i);
+        i++;
+    }
+    return 0;
+ }
